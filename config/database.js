14524159152a0554de1e789e45635c32c69fb01a
JUriasR <juriasr@gmail.com>
2021-07-27 23:25:12 -0600

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,18 +1,43 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'sys'),
-        username: env('DATABASE_USERNAME', 'xuryaz'),
-        password: env('DATABASE_PASSWORD', 's3gUr0$'),
-        ssl: env.bool('DATABASE_SSL', false),
+        client: 'postgres',
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        username: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
-      options: {}
+      options: {
+        ssl: true,
+      },
     },
   },
 });
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'mysql',
+//         host: env('DATABASE_HOST', '127.0.0.1'),
+//         port: env.int('DATABASE_PORT', 3306),
+//         database: env('DATABASE_NAME', 'sys'),
+//         username: env('DATABASE_USERNAME', 'xuryaz'),
+//         password: env('DATABASE_PASSWORD', 's3gUr0$'),
+//         ssl: env.bool('DATABASE_SSL', false),
+//       },
+//       options: {}
+//     },
+//   },
+// });
--- config/server.js
+++ config/server.js
@@ -1,9 +1,13 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '0c9a39235aac48184e81cafaf58676b1'),
-    },
-  },
+  url: env('MY_HEROKU_URL'),
 });
+
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   admin: {
+//     auth: {
+//       secret: env('ADMIN_JWT_SECRET', '0c9a39235aac48184e81cafaf58676b1'),
+//     },
+//   },
+// });
